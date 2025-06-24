5177437d216f4528d7939531296519dec4ea79c6
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-24 18:35:09 +0530

    Initial commit

--- api-routes/app/page.tsx
+++ api-routes/app/page.tsx
@@ -1,10 +1,37 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { log } from "console";
 
 export default function Home() {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleclk = async () => {
+    // let data = {
+    //   name: "Sayandeep",
+    //   role: "Artist",
+    // };
+    let data = name;
+    let a = await fetch("/api/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    let res = await a.json();
+    console.log(res);
+    setName("");
+  };
+
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      
+    <div className="w-full flex items-center flex-col mt-48">
+      <h1>API AND ROUTED</h1>
+      <div className="flex gap-2 mt-4">
+        <input type="text" onChange={handleChange} value={name}  className="bg-zinc-900 p-1 border border-white"/>
+      <button className="border border-white rounded-md p-1" onClick={handleclk}>click me</button>
+      </div>
     </div>
   );
 }
